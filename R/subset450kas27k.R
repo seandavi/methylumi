98470dda531d77baf72850af8fa5bf53634fcbb2
sdavis2@mail.nih.gov <sdavis2@mail.nih.gov@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-10-17 00:16:53 +0000

    Most recent changed by Tim
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/methylumi@59315 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/subset450kas27k.R
+++ R/subset450kas27k.R
@@ -1,16 +1,16 @@
-subset.common.probes <- function(x, ...) { # {{{
-  if (length(list(...)) > 0) do.call(subset.common.probes(list(...)))
-  else if(annotation(x) == 'IlluminaHumanMethylation27k') subset.27k.as.450k(x)
-  else if(annotation(x) == 'IlluminaHumanMethylation450k') subset.450k.as.27k(x)
+subsetCommonProbes <- function(x, ...) { # {{{
+  if (length(list(...)) > 0) do.call(subsetCommonProbes(list(...)))
+  else if(annotation(x) == 'IlluminaHumanMethylation27k') subset27kas450k(x)
+  else if(annotation(x) == 'IlluminaHumanMethylation450k') subset450kas27k(x)
   else stop(paste("Don't know how to handle chip type", annotation(x)))
 } # }}}
-subset.450k.as.27k <- function(x) { # {{{
+subset450kas27k <- function(x) { # {{{
   require(IlluminaHumanMethylation450k.db)
   stopifnot(annotation(x) == 'IlluminaHumanMethylation450k')
   if(!('platform' %in% varLabels(x))) x$platform = 'HumanMethylation450'
   return(x[as.character(unlist(IlluminaHumanMethylation450k_get27k())),])
 } # }}}
-subset.27k.as.450k <- function(x) { # {{{
+subset27kas450k <- function(x) { # {{{
   require(IlluminaHumanMethylation450k.db)
   stopifnot(annotation(x) == 'IlluminaHumanMethylation27k')
   if(!('platform' %in% varLabels(x))) x$platform = 'HumanMethylation27'
