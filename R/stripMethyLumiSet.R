9ca1ef10078c56cda4d781abf80cbad0d2d6548d
Sean Davis <sdavis2@mail.nih.gov>
2011-10-17 00:16:53 +0000

    Most recent changed by Tim
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/methylumi@59315 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/stripMethyLumiSet.R
+++ R/stripMethyLumiSet.R
@@ -1,67 +1,67 @@
 ## FIXME: the real answer is to farm the data out to ff or hdf5, like Benilton
-stripBeadNs <- function(from) { # {{{ make 450k datasets smaller
-  if( 'methylated.N' %in% assayDataElementNames(from) ||
-      'unmethylated.N' %in% assayDataElementNames(from) ) { 
+stripBeadNs <- function(object) { # {{{ make 450k datasets smaller
+  if( 'methylated.N' %in% assayDataElementNames(object) ||
+      'unmethylated.N' %in% assayDataElementNames(object) ) { 
     history.submitted <- as.character(Sys.time())
-    aData <- assayData(from)
+    aData <- assayData(object)
     storageMode(aData) <- "environment"
     rm('unmethylated.N', envir=aData)
     rm('methylated.N', envir=aData)
     storageMode(aData) <- "lockedEnvironment"
-    assayData(from) <- aData
+    assayData(object) <- aData
     history.finished <- as.character(Sys.time())
     history.command <- 'Removed probe-level bead numbers from assayData.'
-    oldHistory <- from@history
+    oldHistory <- object@history
     newHistory <- data.frame(submitted=history.submitted, 
                              finished=history.finished,
                              command=history.command)
-    if(is(from, 'MethyLumiM')) newHistory$lumiVersion = NA
-    from@history<- rbind(oldHistory, newHistory)
+    if(is(object, 'MethyLumiM')) newHistory$lumiVersion = NA
+    object@history<- rbind(oldHistory, newHistory)
   }
-  return(from)
+  return(object)
 } # }}}
-stripBeadSDs <- function(from) { # {{{
-  if( 'methylated.SD' %in% assayDataElementNames(from) ||
-      'unmethylated.SD' %in% assayDataElementNames(from) ) { 
+stripBeadSDs <- function(object) { # {{{
+  if( 'methylated.SD' %in% assayDataElementNames(object) ||
+      'unmethylated.SD' %in% assayDataElementNames(object) ) { 
     history.submitted <- as.character(Sys.time())
-    aData <- assayData(from)
+    aData <- assayData(object)
     storageMode(aData) <- "environment"
     rm('unmethylated.SD', envir=aData)
     rm('methylated.SD', envir=aData)
     storageMode(aData) <- "lockedEnvironment"
-    assayData(from) <- aData
+    assayData(object) <- aData
     history.finished <- as.character(Sys.time())
     history.command <- 'Removed probe-level stderrs from assayData.'
-    oldHistory <- from@history
+    oldHistory <- object@history
     newHistory <- data.frame(submitted=history.submitted, 
                              finished=history.finished,
                              command=history.command)
-    if(is(from, 'MethyLumiM')) newHistory$lumiVersion = NA
-    from@history<- rbind(oldHistory, newHistory)
+    if(is(object, 'MethyLumiM')) newHistory$lumiVersion = NA
+    object@history<- rbind(oldHistory, newHistory)
   }
-  return(from)
+  return(object)
 } # }}} 
-stripOOB <- function(from) { # {{{
-  if( 'methylated.OOB' %in% assayDataElementNames(from) ||
-      'unmethylated.OOB' %in% assayDataElementNames(from) ) { 
+stripOOB <- function(object) { # {{{
+  if( 'methylated.OOB' %in% assayDataElementNames(object) ||
+      'unmethylated.OOB' %in% assayDataElementNames(object) ) { 
     history.submitted <- as.character(Sys.time())
-    aData <- assayData(from)
+    aData <- assayData(object)
     storageMode(aData) <- "environment"
     rm('unmethylated.OOB', envir=aData)
     rm('methylated.OOB', envir=aData)
     storageMode(aData) <- "lockedEnvironment"
-    assayData(from) <- aData
+    assayData(object) <- aData
     history.finished <- as.character(Sys.time())
     history.command <- 'Removed out-of-band intensities from assayData.'
-    oldHistory <- from@history
+    oldHistory <- object@history
     newHistory <- data.frame(submitted=history.submitted, 
                              finished=history.finished,
                              command=history.command)
-    if(is(from, 'MethyLumiM')) newHistory$lumiVersion = NA
-    from@history<- rbind(oldHistory, newHistory)
+    if(is(object, 'MethyLumiM')) newHistory$lumiVersion = NA
+    object@history<- rbind(oldHistory, newHistory)
   }
-  return(from)
+  return(object)
 } # }}}
-stripMethyLumiSet <- function(from) { # {{{
-  return(stripOOB(stripBeadSDs(stripBeadNs(from))))
+stripMethyLumiSet <- function(object) { # {{{
+  return(stripOOB(stripBeadSDs(stripBeadNs(object))))
 } # }}}
