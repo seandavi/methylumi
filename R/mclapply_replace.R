25995362a4d5b4ae3e8d17989f2ce8bd620d2bff
sdavis2@mail.nih.gov <sdavis2@mail.nih.gov@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-01-11 12:52:48 +0000

    Merge branch 'master' into local/trunk
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/methylumi@61985 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/mclapply_replace.R
+++ R/mclapply_replace.R
@@ -1,7 +1,9 @@
 ### Replace mcapply with reasonable default that works on windows.
 .mclapply <- function(...) {
-  if(.Platform$OS.type=='unix') {
-    require(parallel)
+  if(require(parallel)) {
+    if(is.null(options("mc.cores")[[1]])) {
+      message("Remember to set options('mc.cores') if you want multiple cores used where possible")
+    }
     return(mclapply(...))
   } else {
     return(lapply(...))
