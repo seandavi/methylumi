d378300bc1f0077080b92eb31e81c8e496d9e449
Sean Davis <seandavi@gmail.com>
2012-01-11 07:39:03 -0500

    Minor changes to mclapply_replace so that we don't hijack the user's computer by taking all cores.  The user should be in charge of setting mc.cores.

--- R/mclapply_replace.R
+++ R/mclapply_replace.R
@@ -1,10 +1,8 @@
 ### Replace mcapply with reasonable default that works on windows.
 .mclapply <- function(...) {
-  if(.Platform$OS.type=='unix') {
-    require(parallel)
+  if(require(parallel)) {
     if(is.null(options("mc.cores")[[1]])) {
-      message("Autodetecting CPU cores...")
-      options("mc.cores"=detectCores())
+      message("Remember to set options('mc.cores') if you want multiple cores used where possible")
     }
     return(mclapply(...))
   } else {
