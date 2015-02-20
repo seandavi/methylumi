7952ba0e000550fe009fb613fd905c96430d2275
Tim Triche <tim.triche@gmail.com>
2015-02-20 05:11:53 +0000

    linted some methods
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/methylumi@99638 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utilities.R
+++ R/utilities.R
@@ -1,13 +1,14 @@
-setClassUnion("methylData", c('MethyLumiSet','MethyLumiM'))
-setClassUnion("NameOrDefault", c('character','missing'))
-setClassUnion("NumericDefault", c('numeric','missing'))
-setClassUnion("LogicalDefault", c('logical','missing'))
+setClassUnion("methylData", c("MethyLumiSet","MethyLumiM"))
+setClassUnion("NameOrDefault", c("character","missing"))
+setClassUnion("NumericDefault", c("numeric","missing"))
+setClassUnion("LogicalDefault", c("logical","missing"))
 
-# after qsummary() in package 'qvalue', but for B-H adjusted p-value summaries
-psummary <- function (pvals, cuts=c(0.001,0.01,0.025,0.05,0.1,1), digits=getOption("digits"), ...) { # {{{
+# after qsummary() in package "qvalue", but for B-H adjusted p-value summaries
+psummary <- function (pvals, cuts=c(0.001,0.01,0.025,0.05,0.1,1), 
+                      digits=getOption("digits"), ...) { # {{{
     cat("Cumulative number of significant p-values:\n")
     cat("\n")
-    bh.p <- p.adjust(pvals, method='BH')
+    bh.p <- p.adjust(pvals, method="BH")
     counts <- sapply(cuts, function(p) c(`p-value` = sum(bh.p < p)))
     names(counts) <- paste("<", cuts, sep = "")
     print(counts)
@@ -17,17 +18,20 @@ psummary <- function (pvals, cuts=c(0.001,0.01,0.025,0.05,0.1,1), digits=getOpti
 
 # get the URL of a Bioconductor package:
 getBiocPkgUrl <- function(pkg) { # {{{
-  paste0("http://www.bioconductor.org/packages/release/bioc/html/",pkg,".html")
+  paste0("http://www.bioconductor.org/packages/release/bioc/html/",
+         pkg,".html")
 } # }}}
 
 # useful for feature selection if not using shrinkage
-sdmax <- function(y) sd(na.omit(y))/sqrt(mean(na.omit(y))*(1-mean(na.omit(y))))
+sdmax <- function(y) {
+  sd(na.omit(y)) / sqrt(mean(na.omit(y)) * (1 - mean(na.omit(y))))
+}
 
 # utility functions I tend to use unconsciously
-shift <- function(x) if(length(x)>1) x[[1]] else x
+shift <- function(x) if (length(x) > 1) x[[1]] else x
 unshift <- function(x,y) append(y,x)
 push <- function(x,y) append(x,y)
-pop <- function(x) if(length(x)>1) x[[length(x)]] else x
+pop <- function(x) if (length(x) > 1) x[[length(x)]] else x
 
 # utility operators
 "%i%" <- intersect
@@ -35,19 +39,20 @@ pop <- function(x) if(length(x)>1) x[[length(x)]] else x
 "%u%" <- union
 
 # using this is a bad habit of mine 
-"%nin%" <- "%notin%" <- function(x,table) match(x, table, nomatch=0)==0
+"%nin%" <- "%notin%" <- function(x,table) match(x, table, nomatch=0) == 0
 
 # the following are rather handy for constructing MethyLumiSets and such
 t.submit <- function() as.character(Sys.time())
 t.finish <- function() as.character(format(Sys.time(), "%H:%M:%S"))
 
 ## handy for grabbing all IDAT files in an Illumina directory
-getBarcodes <- function(path='.') { # {{{
+getBarcodes <- function(path=".") { # {{{
   oldwd <- getwd()
   setwd(path)
-  barcds <- unique(gsub('_(Red|Grn).idat','',list.files(path=path,patt='idat')))
+  barcds <- unique(gsub("_(Red|Grn).idat","",
+                        list.files(path=path,patt="idat")))
   setwd(oldwd)
-  names(barcds) = barcds
+  names(barcds) <- barcds
   return(barcds)
 } # }}}
 
@@ -55,11 +60,11 @@ getBarcodes <- function(path='.') { # {{{
 beta.mme <- function(x, w=NULL, ...) { # {{{
   if(any(is.na(x))) stop("Cannot handle NA values!")
   ## a lie: we COULD, but let the user call impute.knn()
-  if(is.null(w)) w <- rep(1/length(x), length(x))
+  if(is.null(w)) w <- rep(1 / length(x), length(x))
   xb <- weighted.mean(x, w, na.rm=T)
-  s2 <- sum(w*((x-xb)**2)) / sum(w)
-  a <- xb * (((xb*(1-xb))/s2)-1)
-  b <- (1-xb) * (((xb*(1-xb))/s2)-1)
+  s2 <- sum(w * ( (x - xb) ** 2)) / sum(w)
+  a <- xb * ( ( (xb * (1 - xb)) / s2) - 1)
+  b <- (1 - xb) * ( ( (xb * (1 - xb)) / s2) - 1)
   mme <- c(a=max(a,0), b=max(b,0))
   return(mme)
 } # }}}
@@ -72,28 +77,28 @@ beta.mode <- function(a, b) { # {{{
   if(all(c(a,b) < 1)) return(NaN)
   if(a < 1) return(1)
   if(b < 1) return(0)
-  return( (a-1) / (a+b-2) )
+  return( (a - 1) / (a + b - 2) )
 } # }}}
 
 ## from Smithson & Verkuilen 2006; shrink to 0.5, the mean, or the mode
 ##
 beta.transform <- function(x, w=NULL, to.mean=TRUE, to.mode=FALSE, s=0.5){ #{{{
 
-  stopifnot( (max(x)<=1) && (min(x)>=0) )
+  stopifnot( (max(x) <= 1) && (min(x) >= 0) )
   n <- length(x)
   if( is.null(w) ) w <- rep(1, n)
-  else w <- (w / (sum(w)/n))
+  else w <- (w / (sum(w) / n))
   if(to.mean) s <- weighted.mean(x, w, na.rm=T)
   if(to.mode) s <- pmax(0.01, pmin(0.99, beta.mode(beta.mme(x, w))))
-  return( ((x*(n-1))+s) / n )
+  return( ( ( x * ( n - 1 ) ) + s) / n )
 
 } # }}}
 
 ## I'm really, really lazy
 tcgaPipeline <- function(IDATs) { 
-  as(stripMethyLumiSet(
-       normalizeMethyLumiSet(
+  as(normalizeMethyLumiSet(
+       stripMethyLumiSet(
          methylumi.bgcorr(
            methylumIDAT(IDATs)))),
-     'SummarizedExperiment')
+     "SummarizedExperiment")
 }
