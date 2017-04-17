80ad4687a9f70000d5c89903f9d4135666036b06
Sean Davis <seandavi@gmail.com>
2017-04-17 14:17:26 -0400

    Removed stray R file outside R directory

--- data/test27k.R
+++
@@ -1,6 +0,0 @@
-## useDevel() if need be
-library(methylumi)
-## biocLite('IlluminaHumanMethylation27k.db') if need be
-mset <- normalizeMethyLumiSet(methylumi.bgcorr(methylumIDAT(getBarcodes())))
-show(mset)
-sessionInfo()
